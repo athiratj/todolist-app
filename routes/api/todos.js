aaff2f067b0150f8fe2e83488aff91ae9d4ca8ac
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-06-01 23:04:13 +0530

    created a post

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //todo model
-const Todo = require('../../models/Todo');
+const Todo = require("../../models/Todo");
 
-router.get('/', (req, res) => {
-    Todo.find()
-        .sort({ date: -1 })
-        .then(todos => res.json(items));
+//Get all todos
+router.get("/", (req, res) => {
+  Todo.find()
+    .sort({ date: -1 })
+    .then((todos) => res.json(todos));
+});
+
+//create a post
+router.post("/", (req, res) => {
+  const newTodo = new Todo({
+    name: req.body.name,
+  });
+    newTodo.save().then(todo => res.json(todo));
 });
 
 module.exports = router;
