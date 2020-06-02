5d2a9b6f7ef986088a93aac656d3c9c145e992a4
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-06-02 15:05:15 +0530

    added the client setup using reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AppNavbar from './components/AppNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <AppNavbar />
     </div>
   );
 }
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,35 @@
+import React,{useState} from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Container,
+  NavLink,
+} from "reactstrap";
+
+const AppNavbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <div>
+      <Navbar color="dark" dark expand="sm" className="mb-5">
+        <Container>
+          <NavbarBrand href="/">Todo List</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="https://github.com/athiratj/todolist-app">GitHub</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+export default AppNavbar;
