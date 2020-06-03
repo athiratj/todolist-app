2ab2b51f983493dec0fcc9d098542a3fa70a94b2
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-06-03 13:58:10 +0530

    moved the state from our TodoList component to a store reducer.

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { v4 as uuidv4 } from "uuid";
+import { connect } from 'react-redux';
+import { getTodos } from '../actions/todoActions';
+import PropTypes from 'prop-types';
 
 class TodoList extends React.Component {
-  state = {
-    todos: [
-      { id: uuidv4(), name: "Eggs" },
-      { id: uuidv4(), name: "Milk" },
-      { id: uuidv4(), name: "Streak" },
-      { id: uuidv4(), name: "Water" },
-    ],
-  };
+  
+  componentDidMount() {
+    this.props.getTodos();
+  }
+
+
   render() {
-    const { todos } = this.state;
+    const { todos } = this.props.todo;
     return (
       <Container>
         <Button
@@ -56,5 +57,11 @@ class TodoList extends React.Component {
     );
   }
 }
-
-export default TodoList;
+TodoList.propTypes = {
+  getTodos: PropTypes.func.isRequired,
+  item:PropTypes.object.isRequired
+}
+const mapStateToProps = (state) => ({
+  todo: state.todo
+});
+export default connect(mapStateToProps, {getTodos})(TodoList);
