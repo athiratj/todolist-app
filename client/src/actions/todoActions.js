f7c8619c0f39efa22fc59c01c9e127bfca00637b
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-06-03 22:44:06 +0530

    brought the loading action to connect to backend routes within redux actions using axios

--- client/src/actions/todoActions.js
+++ client/src/actions/todoActions.js
@@ -1,20 +1,40 @@
-import { GET_TODOS, ADD_TODO, DELETE_TODO } from "./types";
+import axios from 'axios';
+import { GET_TODOS, ADD_TODO, DELETE_TODO, TODOS_LOADING } from "./types";
 
-export const getTodos = () => {
-  return {
-    type: GET_TODOS,
-  };
+export const getTodos = ()=> dispatch=> {
+    dispatch(setTodosLoading());
+    axios
+    .get('/api/todos')
+    .then(res =>
+      dispatch({
+        type: GET_TODOS,
+        payload: res.data
+      })
+    )
 };
-export const addTodo = (todo) => {
-  return {
-    type: ADD_TODO,
-    payload: todo,
-  };
+export const addTodo = (todo) =>dispatch=> {
+    axios
+    .post('/api/todos', todo)
+    .then(res =>
+      dispatch({
+        type: ADD_TODO,
+        payload: res.data
+      })
+    )
 };
 
-export const deleteTodo = (id) => {
-  return {
-    type: DELETE_TODO,
-    payload: id,
-  };
+export const deleteTodo = (id) =>dispatch=> {
+    axios
+    .delete(`/api/todos/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_TODO,
+        payload: id
+      })
+    )
 };
+export const setTodosLoading = () => {
+    return {
+      type:TODOS_LOADING
+    };
+  };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
 export const GET_TODOS = 'GET_TODOS';
 export const ADD_TODO = 'ADD_TODO';
 export const DELETE_TODO = 'DELETE_TODO';
+export const TODOS_LOADING = 'TODOS_LOADING';
