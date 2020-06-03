42ebea811b875fa4a1f6040db99eaa99aa030cfd
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-06-03 21:44:59 +0530

    brought the add action to TodoModal And delete action to TodoList

--- client/src/actions/todoActions.js
+++ client/src/actions/todoActions.js
@@ -1,7 +1,20 @@
 import { GET_TODOS, ADD_TODO, DELETE_TODO } from "./types";
 
 export const getTodos = () => {
-    return {
-        type:GET_TODOS
-    }
-}
+  return {
+    type: GET_TODOS,
+  };
+};
+export const addTodo = (todo) => {
+  return {
+    type: ADD_TODO,
+    payload: todo,
+  };
+};
+
+export const deleteTodo = (id) => {
+  return {
+    type: DELETE_TODO,
+    payload: id,
+  };
+};
