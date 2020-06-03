66a3e3a6858f38761a5f7f1cad01f8de0ac8ea7c
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-06-03 22:49:52 +0530

    removed uuids and added loading according to redux actions

--- client/src/reducers/todoReducer.js
+++ client/src/reducers/todoReducer.js
@@ -1,30 +1,37 @@
-import { v4 as uuidv4 } from "uuid";
-import { GET_TODOS, ADD_TODO, DELETE_TODO } from "../actions/types";
+import {
+  GET_TODOS,
+  ADD_TODO,
+  DELETE_TODO,
+  TODOS_LOADING,
+} from "../actions/types";
 
 const initialState = {
-  todos: [
-    { id: uuidv4(), name: "Clean room" },
-    { id: uuidv4(), name: "Complete the project" },
-    { id: uuidv4(), name: "Prepare for exam" },
-    { id: uuidv4(), name: "Water the plants" },
-  ],
+  todos: [],
+  loading: false,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case GET_TODOS:
       return {
-        ...state,
+          ...state,
+        todos: action.payload,
+          loading:false
       };
     case DELETE_TODO:
       return {
         ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.payload),
+        todos: state.todos.filter((todo) => todo._id !== action.payload),
       };
     case ADD_TODO:
       return {
         ...state,
         todos: [action.payload, ...state.todos],
+      };
+    case TODOS_LOADING:
+      return {
+        ...state,
+        loading: true,
       };
     default:
       return state;
