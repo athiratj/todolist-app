c9f85c68711a52655af39352a35f6b92dea0b0dd
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-06-03 21:39:04 +0530

    added the TodoModal component to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import AppNavbar from './components/AppNavbar';
-import TodoList from './components/TodoList';
+import React from "react";
+import AppNavbar from "./components/AppNavbar";
+import TodoList from "./components/TodoList";
+import TodoModal from "./components/TodoModal";
+import { Container } from "reactstrap";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-      <AppNavbar />
-      <TodoList />
-    </div>
+        <AppNavbar />
+        <Container>
+          <TodoModal />
+          <TodoList />
+        </Container>
+      </div>
     </Provider>
-    
   );
 }
 
